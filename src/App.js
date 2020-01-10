29cbdd0c75cabd90756a7a78162c13d8f53ebe93
Thor Torvund <thor.torvund@gmail.com>
2020-01-10 13:58:30 +0100

    test ny branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Thor Tester - Hi there!
+          Thor Tester - Hi there, I am changed again!
        </p>
       </div>
     );
